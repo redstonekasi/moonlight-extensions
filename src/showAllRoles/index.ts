cb1e92932755f06045a9381cd97eac550962f627
redstonekasi <kasi@hanneskann.de>
2025-01-07 21:59:07 +0100

    showAllRoles: initial commit

---
+++ src/showAllRoles/index.ts
@@ -0,0 +1,18 @@
+import { ExtensionWebExports } from "@moonlight-mod/types";
+
+export const patches: ExtensionWebExports["patches"] = [
+  {
+    find: ':"EXPAND_ROLES"',
+    replace: { // remove condition on return for layout calc
+      match: /(?<=\.useLayoutEffect\(\(\)=>{)if\(.\)/,
+      replacement: "",
+    },
+  },
+  {
+    find: ':"EXPAND_ROLES"',
+    replace: { // if above ever fails, here's an extremely specific patch
+      match: /(?<=roles:(.).+?)\[(.),(.)\]=.\.useState\(!1\)(?=.+?\2\?\1)/,
+      replacement: (_1, _2, value, setter) => `${value}=false,${setter}=()=>{}`,
+    },
+  },
+];
