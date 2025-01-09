0e5128c4f3914c3bd4810934c15debf6a9298a96
redstonekasi <kasi@hanneskann.de>
2025-01-10 00:10:45 +0100

    customIdleTimeout: initial commit

---
+++ src/customIdleTimeout/index.ts
@@ -0,0 +1,13 @@
+import { ExtensionWebExports } from "@moonlight-mod/types";
+
+// TODO: that should be a normal number input
+
+export const patches: ExtensionWebExports["patches"] = [
+  {
+    find: ':"OVERLAY_SET_NOT_IDLE"',
+    replace: {
+      match: /(?<=Date\.now\(\)-\i>)\i\.\i(?=\|\|)/,
+      replacement: (c: string) => `(parseInt(moonlight.getConfigOption("customIdleTimeout","timeout"))||${c})`,
+    }
+  }
+];
