92bb6b0a75ffbe314f19b22845064dc832a4b2a4
redstonekasi <kasi@hanneskann.de>
2025-01-10 00:10:58 +0100

    noHelp: initial commit

---
+++ src/noHelp/index.ts
@@ -0,0 +1,11 @@
+import { ExtensionWebExports } from "@moonlight-mod/types";
+
+export const patches: ExtensionWebExports["patches"] = [
+  {
+    find: '"mod+shift+h"',
+    replace: {
+      match: /,"f1"/,
+      replacement: ""
+    }
+  }
+];
